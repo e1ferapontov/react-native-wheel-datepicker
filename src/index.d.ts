2f1bd7b32f2b5f0047ef443bafca8f748e2aeb34
Evgeniy Ferapontov <34385302+e1ferapontov@users.noreply.github.com>
2019-11-01 17:59:01 +0300

    renamed for similarity with RN picker types

--- src/index.d.ts
+++ src/index.d.ts
@@ -9,9 +9,9 @@ declare module 'react-native-wheel-picker' {
 
     export type PickerDataItem = string | number | PickerDataItemObject;
 
-    export type WheelPickerProps = ReactNative.PickerProps & {
+    export type PickerProps = ReactNative.PickerProps & {
         pickerData: PickerDataItem[];
     };
 
-    export type Picker = React.FC<WheelPickerProps>;
+    export type Picker = React.FC<PickerProps>;
 }
